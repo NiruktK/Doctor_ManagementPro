2b0fbd60c3d98b600c4546ddcb865d46b6f0058e
NiruktK <122807176+NiruktK@users.noreply.github.com>
2024-05-31 14:45:41 +0530

    Add files via upload

---
+++ DoctorManagementCrudApplication.java
@@ -0,0 +1,14 @@
+package com.drmanagement.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorManagementCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorManagementCrudApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
